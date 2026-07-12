4caeedc630238765fb3cadde735d1324afc3b3f7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:57:55 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+data "azurerm_key_vault_secret" "developer_app_insights_api_key" {
+  for_each     = { for k, v in var.bot_service_azure_bots : k => v if v.developer_app_insights_api_key_key_vault_id != null && v.developer_app_insights_api_key_key_vault_secret_name != null }
+  name         = each.value.developer_app_insights_api_key_key_vault_secret_name
+  key_vault_id = each.value.developer_app_insights_api_key_key_vault_id
+}
+data "azurerm_key_vault_secret" "luis_key" {
+  for_each     = { for k, v in var.bot_service_azure_bots : k => v if v.luis_key_key_vault_id != null && v.luis_key_key_vault_secret_name != null }
+  name         = each.value.luis_key_key_vault_secret_name
+  key_vault_id = each.value.luis_key_key_vault_id
+}
 resource "azurerm_bot_service_azure_bot" "bot_service_azure_bots" {
   for_each = var.bot_service_azure_bots
 
@@ -10,7 +20,7 @@ resource "azurerm_bot_service_azure_bot" "bot_service_azure_bots" {
   microsoft_app_type                    = each.value.microsoft_app_type
   microsoft_app_tenant_id               = each.value.microsoft_app_tenant_id
   microsoft_app_msi_id                  = each.value.microsoft_app_msi_id
-  luis_key                              = each.value.luis_key
+  luis_key                              = each.value.luis_key != null ? each.value.luis_key : try(data.azurerm_key_vault_secret.luis_key[each.key].value, null)
   luis_app_ids                          = each.value.luis_app_ids
   local_authentication_enabled          = each.value.local_authentication_enabled
   endpoint                              = each.value.endpoint
@@ -18,7 +28,7 @@ resource "azurerm_bot_service_azure_bot" "bot_service_azure_bots" {
   display_name                          = each.value.display_name
   developer_app_insights_key            = each.value.developer_app_insights_key
   developer_app_insights_application_id = each.value.developer_app_insights_application_id
-  developer_app_insights_api_key        = each.value.developer_app_insights_api_key
+  developer_app_insights_api_key        = each.value.developer_app_insights_api_key != null ? each.value.developer_app_insights_api_key : try(data.azurerm_key_vault_secret.developer_app_insights_api_key[each.key].value, null)
   cmk_key_vault_key_url                 = each.value.cmk_key_vault_key_url
   icon_url                              = each.value.icon_url
   tags                                  = each.value.tags
--- variables.tf
+++ variables.tf
@@ -10,6 +10,8 @@ Required:
 Optional:
     - cmk_key_vault_key_url
     - developer_app_insights_api_key
+    - developer_app_insights_api_key_key_vault_id (alternative to developer_app_insights_api_key - read from Key Vault instead)
+    - developer_app_insights_api_key_key_vault_secret_name (alternative to developer_app_insights_api_key - read from Key Vault instead)
     - developer_app_insights_application_id
     - developer_app_insights_key
     - display_name
@@ -18,6 +20,8 @@ Optional:
     - local_authentication_enabled
     - luis_app_ids
     - luis_key
+    - luis_key_key_vault_id (alternative to luis_key - read from Key Vault instead)
+    - luis_key_key_vault_secret_name (alternative to luis_key - read from Key Vault instead)
     - microsoft_app_msi_id
     - microsoft_app_tenant_id
     - microsoft_app_type
@@ -27,27 +31,31 @@ Optional:
 EOT
 
   type = map(object({
-    location                              = string
-    microsoft_app_id                      = string
-    name                                  = string
-    resource_group_name                   = string
-    sku                                   = string
-    public_network_access_enabled         = optional(bool) # Default: true
-    microsoft_app_type                    = optional(string)
-    microsoft_app_tenant_id               = optional(string)
-    microsoft_app_msi_id                  = optional(string)
-    luis_key                              = optional(string)
-    luis_app_ids                          = optional(list(string))
-    local_authentication_enabled          = optional(bool) # Default: true
-    endpoint                              = optional(string)
-    streaming_endpoint_enabled            = optional(bool) # Default: false
-    display_name                          = optional(string)
-    developer_app_insights_key            = optional(string)
-    developer_app_insights_application_id = optional(string)
-    developer_app_insights_api_key        = optional(string)
-    cmk_key_vault_key_url                 = optional(string)
-    icon_url                              = optional(string) # Default: "https://docs.botframework.com/static/devportal/client/images/bot-framework-default.png"
-    tags                                  = optional(map(string))
+    location                                             = string
+    microsoft_app_id                                     = string
+    name                                                 = string
+    resource_group_name                                  = string
+    sku                                                  = string
+    public_network_access_enabled                        = optional(bool) # Default: true
+    microsoft_app_type                                   = optional(string)
+    microsoft_app_tenant_id                              = optional(string)
+    microsoft_app_msi_id                                 = optional(string)
+    luis_key                                             = optional(string)
+    luis_key_key_vault_id                                = optional(string)
+    luis_key_key_vault_secret_name                       = optional(string)
+    luis_app_ids                                         = optional(list(string))
+    local_authentication_enabled                         = optional(bool) # Default: true
+    endpoint                                             = optional(string)
+    streaming_endpoint_enabled                           = optional(bool) # Default: false
+    display_name                                         = optional(string)
+    developer_app_insights_key                           = optional(string)
+    developer_app_insights_application_id                = optional(string)
+    developer_app_insights_api_key                       = optional(string)
+    developer_app_insights_api_key_key_vault_id          = optional(string)
+    developer_app_insights_api_key_key_vault_secret_name = optional(string)
+    cmk_key_vault_key_url                                = optional(string)
+    icon_url                                             = optional(string) # Default: "https://docs.botframework.com/static/devportal/client/images/bot-framework-default.png"
+    tags                                                 = optional(map(string))
   }))
 }
 
